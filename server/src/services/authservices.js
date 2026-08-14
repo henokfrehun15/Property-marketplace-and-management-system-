64ad0ec12691617cd2dc4a21d3c4fe084fc28118
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 15:47:22 +0300

    added jwt and created login authentication

--- server/src/services/authservices.js
+++ server/src/services/authservices.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const registerUser = async ({ name, email, password }) => {
@@ -17,7 +18,40 @@ const registerUser = async ({ name, email, password }) => {
 
   return user;
 };
+const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email }).select("+password");
 
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isPasswordCorrect) {
+    throw new Error("Invalid email or password");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d"
+    }
+  );
+
+  return {
+    user,
+    token
+  };
+};
+ 
 module.exports = {
-  registerUser
+  registerUser,
+  loginUser
 };
