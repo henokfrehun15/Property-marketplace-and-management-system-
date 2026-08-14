9f14d2b9bb7ad1e41edb2bedbeb0bc2fbb102036
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 16:53:03 +0300

    added getme code to check if middleware works

--- server/src/controllers/authcontrollers.js
+++ server/src/controllers/authcontrollers.js
@@ -58,8 +58,21 @@ const login = async (req, res) => {
     });
   }
 };
-
+const getMe = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      user: req.user
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
+  }
+};
 module.exports = {
   register,
-  login
+  login,      
+  getMe
 };
