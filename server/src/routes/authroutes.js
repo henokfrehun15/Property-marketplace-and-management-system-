0a9ca9ed026dd915c1997dcdd1ddfbf0de44d31b
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 10:56:25 +0300

    updatde path name inconsisency

--- server/src/routes/authroutes.js
+++ server/src/routes/authroutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const { register } = require("../controllers/authController");
+const { register } = require("../controllers/authcontrollers");
 const router = express.Router();
 
 router.post("/register", register);
 
 module.exports = router;
-   
