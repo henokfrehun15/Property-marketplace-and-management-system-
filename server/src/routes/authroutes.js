ef70a10ef3e554883f3e27c6be0fbf26cc25fa27
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 16:54:54 +0300

    added getme in route to test middleware

--- server/src/routes/authroutes.js
+++ server/src/routes/authroutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { 
     register, 
-    login 
+    login,
+    getMe
 } = require("../controllers/authcontrollers");
+const protect = require("../middleware/authmiddleware");
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
-
+router.get("/me",protect, getMe);
 module.exports = router;
