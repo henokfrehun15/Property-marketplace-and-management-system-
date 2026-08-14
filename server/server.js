a2ecf3f7b6d04a8892fb033db26b8aa6ff0f4e89
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 09:14:43 +0300

    removed testing codes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
-const User = require("./src/models/user");
 dotenv.config();
 
 const app = express();
@@ -21,23 +20,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-});
-app.post("/api/test-user", async (req, res) => {
-  try {
-    const user = await User.create({
-      name: "Test User",
-      email: "test@propertyhub.com",
-      password: "123456"
-    });
-
-    res.status(201).json({
-      success: true,
-      user
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message
-    });
-  }
 });
