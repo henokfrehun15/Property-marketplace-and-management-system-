a138fcc9acc1b034e4380b33cbf555dd74f2fc60
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 16:17:06 +0300

    added login to routes

--- server/src/routes/authroutes.js
+++ server/src/routes/authroutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { register } = require("../controllers/authcontrollers");
+const { 
+    register, 
+    login 
+} = require("../controllers/authcontrollers");
 const router = express.Router();
-
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
