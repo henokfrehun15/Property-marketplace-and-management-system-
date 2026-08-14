4b8736d4ccc7be13cad8cd9f2a181dfb99d0e3b6
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-14 10:55:14 +0300

    updated naming and added json output that shows user registered

--- server/src/controllers/authcontrollers.js
+++ server/src/controllers/authcontrollers.js
@@ -1,8 +1,31 @@
+const { registerUser } = require("../services/authservices");
+
 const register = async (req, res) => {
-  res.status(201).json({
-    success: true,
-    message: "User registration controller reached"
-  });
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await registerUser({
+      name,
+      email,
+      password
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user:{
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+  }
 };
 
 module.exports = {
