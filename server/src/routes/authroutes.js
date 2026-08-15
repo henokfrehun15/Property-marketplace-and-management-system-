8e89a028ae1f6907c7f6a523499de19960cb1e60
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-15 23:12:51 +0300

    added authorization test to the route and also middleware

--- server/src/routes/authroutes.js
+++ server/src/routes/authroutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { 
     register, 
     login,
-    getMe
+    getMe,
+    ownerTest  
 } = require("../controllers/authcontrollers");
 const protect = require("../middleware/authmiddleware");
+const authorize = require("../middleware/rolemiddleware");
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me",protect, getMe);
+router.get("/owner-test", protect, authorize("owner"), ownerTest);
 module.exports = router;
