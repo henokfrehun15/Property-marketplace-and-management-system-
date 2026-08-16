2707a57d1acd4743fccfb089957ac30fa77f9a57
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-16 12:15:40 +0300

    added the routes of properties to create a property in the server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
 const authroutes = require("./src/routes/authroutes");
-
+const propertyRoutes = require("./src/routes/propertyroutes");
 dotenv.config();
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 app.use(express.json());
 app.use("/api/auth", authroutes);
-
+app.use("/api/properties", propertyRoutes);
 app.get("/api/health", (req, res) => {
   res.json({
     success: true,
