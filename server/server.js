e5d19247b5bd1cb79379e8002722b005c008acbf
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-17 14:29:16 +0300

    add favorite routes in the server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
 const authroutes = require("./src/routes/authroutes");
 const propertyRoutes = require("./src/routes/propertyroutes");
+const favoriteRoutes = require("./src/routes/favoriteroutes");
 dotenv.config();
 
 const app = express();
@@ -17,7 +18,7 @@ app.get("/api/health", (req, res) => {
     message: "PropertyHub API is running"
   });
 }); 
-
+app.use("/api/favorites", favoriteRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
