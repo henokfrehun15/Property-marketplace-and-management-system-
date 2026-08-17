404501085d54023103bd2b0e7bf675e04edfeaff
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-17 15:52:20 +0300

    removed try catch blocks

--- server/src/controllers/propertycontrollers.js
+++ server/src/controllers/propertycontrollers.js
@@ -7,7 +7,6 @@ const {
  } = require("../services/propertyservices");
 
 const create = async (req, res) => {
-  try {
     const property = await createProperty(
       req.body,
       req.user.userId
@@ -18,45 +17,24 @@ const create = async (req, res) => {
       message: "Property created successfully",
       property
     });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
 };
 const getAll = async (req, res) => {
-  try {
     const result = await getAllProperties(req.query);
 
     res.status(200).json({
       success: true,
       ...result
     });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to fetch properties"
-    });
-  }
 };
 const getProperty = async (req, res) => {
-  try {
     const property = await getPropertyById(req.params.id);
 
     res.status(200).json({
       success: true,
       property
     });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      message: error.message
-    });
-  }
 };
 const update = async (req, res) => {
-  try {
     const property = await updateProperty(
       req.params.id,
       req.body,
@@ -69,15 +47,8 @@ const update = async (req, res) => {
       message: "Property updated successfully",
       property
     });
-  } catch (error) {
-    res.status(error.statusCode || 400).json({
-      success: false,
-      message: error.message
-    });
-  }
 };
 const remove = async (req, res) => {
-  try {
     await deleteProperty(
       req.params.id,
       req.user.userId,
@@ -88,12 +59,6 @@ const remove = async (req, res) => {
       success: true,
       message: "Property deleted successfully"
     });
-  } catch (error) {
-    res.status(error.statusCode || 400).json({
-      success: false,
-      message: error.message
-    });
-  }
 };
 module.exports = {
   create,
