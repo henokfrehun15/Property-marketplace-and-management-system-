864d3b1d1d6857bf9a15634d1c669e1337f2d7c7
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-19 16:37:51 +0300

    add cors  because of port

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./src/config/db");
 const authroutes = require("./src/routes/authroutes");
 const propertyRoutes = require("./src/routes/propertyroutes");
@@ -10,7 +11,9 @@ const imageRoutes = require("./src/routes/imageroutes");
 
 
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
 connectDB();
 app.use(express.json());
 app.use("/api/auth", authroutes);
