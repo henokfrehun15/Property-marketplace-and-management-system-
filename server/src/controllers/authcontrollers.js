bc0eea20521b31a3476659d447cfcaeba19cedb2
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-20 12:35:44 +0300

    update getme so it send username and necessary data to the front end

--- server/src/controllers/authcontrollers.js
+++ server/src/controllers/authcontrollers.js
@@ -58,13 +58,27 @@ const login = async (req, res) => {
     });
   }
 };
+
 const getMe = async (req, res) => {
   try {
+    const user = await User.findById(req.user.userId)
+      .select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
     res.status(200).json({
       success: true,
-      user: req.user
+      user
     });
+
   } catch (error) {
+    console.error(error);
+
     res.status(500).json({
       success: false,
       message: "Server error"
