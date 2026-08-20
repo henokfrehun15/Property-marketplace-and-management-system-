ee8395db0f2f1ae63e179806ddb5c2dcf10b6a55
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-20 17:24:30 +0300

    update main to include favorite provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext.jsx";
-
+import { FavoritesProvider } from "./context/FavoritesProvider.jsx";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </AuthProvider>
   </StrictMode>,
 )
