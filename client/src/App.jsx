2d52076a3050d4453384c509c55fb5257d828a7b
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-20 12:49:04 +0300

    add route to favorite ,login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import PropertyDetails from "./pages/propertyDetails";
 import Navbar from "./components/Navbar";
 import Properties from "./pages/property";
-
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoutes";
+import Favorites from "./pages/Favorites";
 function Home() {
   return (
     <div>
@@ -21,9 +23,17 @@ function App() {
         <Route path="/" element={<Home />} />
 
         <Route
-          path="/properties"
-          element={<Properties />}
+          path="/properties" element={<Properties />}
         />
+        <Route
+          path="/properties/:id" element={<PropertyDetails />}
+/>      <Route
+          path="/login" element={<Login />}
+/>      <Route element={<ProtectedRoute />}>
+          <Route
+            path="/Favorites" element={<Favorites />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
