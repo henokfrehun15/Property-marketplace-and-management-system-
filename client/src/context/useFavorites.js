2b3dc8bac0b4eb0aaa78dd4d6a445ffa7c809ee0
henokfrehun15 <henokfrehun15@gmail.com>
2026-08-20 17:21:22 +0300

    add use favorites to use favorites

---
+++ client/src/context/useFavorites.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+
+import FavoritesContext from "./FavoritesContext";
+
+export function useFavorites() {
+  const context = useContext(FavoritesContext);
+
+  if (!context) {
+    throw new Error(
+      "useFavorites must be used inside FavoritesProvider"
+    );
+  }
+
+  return context;
+}
